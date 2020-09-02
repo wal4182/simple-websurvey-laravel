c7e02d2ec18e9382a3533bf64b123be0f9b3307c
wal4182 <muhammad.awaludin@students.amikom.ac.id>
2020-09-02 13:15:38 +0700

    Added condition in function store SurveyController

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -20,19 +20,59 @@ class SurveyController extends Controller
     //
     public function store(Request $request)
     {
-        $fav_food = join(',', $request->input('fav_food'));
-        $fav_drink = join(',', $request->input('fav_drink'));
+        $input_fav_food = $request->input('fav_food');
+        $input_fav_drink = $request->input('fav_drink');
+        
+        if(empty($input_fav_food) && !empty($input_fav_drink)){
+            $fav_drink = join(',', $request->input('fav_drink'));
+            DB::table('surveys')->insert([
+                'name' => $request->name,
+                'email' => $request->email,
+                'gender' => $request->gender,
+                'service_rate' => $request->service_rate,
+                'fav_food' => 'Tidak Ada',
+                'fav_drink' => $fav_drink,
+                'recommended' => $request->recommended,
+                'suggestion'=> $request->suggestion,
+            ]);
+        }if(empty($input_fav_drink) && !empty($input_fav_food)){
+            $fav_food = join(',', $request->input('fav_food'));
+            DB::table('surveys')->insert([
+                'name' => $request->name,
+                'email' => $request->email,
+                'gender' => $request->gender,
+                'service_rate' => $request->service_rate,
+                'fav_food' => $fav_food,
+                'fav_drink' => 'Tidak Ada',
+                'recommended' => $request->recommended,
+                'suggestion'=> $request->suggestion,
+            ]);
+        }if(empty($input_fav_food) && empty($input_fav_drink)){
+            DB::table('surveys')->insert([
+                'name' => $request->name,
+                'email' => $request->email,
+                'gender' => $request->gender,
+                'service_rate' => $request->service_rate,
+                'fav_food' => 'Tidak Ada',
+                'fav_drink' => 'Tidak Ada',
+                'recommended' => $request->recommended,
+                'suggestion'=> $request->suggestion,
+            ]);
+        }else{
 
-        DB::table('surveys')->insert([
-            'name' => $request->name,
-            'email' => $request->email,
-            'gender' => $request->gender,
-            'service_rate' => $request->service_rate,
-            'fav_food' => $fav_food,
-            'fav_drink' => $fav_drink,
-            'recommended' => $request->recommended,
-            'suggestion'=> $request->suggestion,
-        ]);
+            $fav_food = join(',', $request->input('fav_food'));
+            $fav_drink = join(',', $request->input('fav_drink'));
+            DB::table('surveys')->insert([
+                'name' => $request->name,
+                'email' => $request->email,
+                'gender' => $request->gender,
+                'service_rate' => $request->service_rate,
+                'fav_food' => $fav_food,
+                'fav_drink' => $fav_drink,
+                'recommended' => $request->recommended,
+                'suggestion'=> $request->suggestion,
+                ]);
+        }
         return redirect('home')->with('success','Survey Berhasil Disimpan');
         
     }
