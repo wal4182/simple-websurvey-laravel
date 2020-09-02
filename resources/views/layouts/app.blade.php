131b2832a5d1a8275781fd43544ea89a2513ec94
wal4182 <muhammad.awaludin@students.amikom.ac.id>
2020-09-02 11:48:27 +0700

    Added css yield to app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    @yield('css')
   </head>
 
   <body>
