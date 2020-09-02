fe2beb2d66ca69473666487e520a36fa1d16c992
wal4182 <muhammad.awaludin@students.amikom.ac.id>
2020-09-02 15:49:02 +0700

    Added user relation into Survey model

--- app/Survey.php
+++ app/Survey.php
@@ -8,6 +8,13 @@ class Survey extends Model
 {
     //
     protected $fillable = [
-        'name', 'email', 'gender', 'service_rate', 'fav_food', 'fav_drink', 'recommended', 'suggestion',
+        'name', 'email', 'gender', 'service_rate', 'fav_food', 'fav_drink', 'recommended', 'suggestion', 'created_at'
     ];
+
+    public $timestamps = true;
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
